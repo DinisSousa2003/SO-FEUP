1272e4d276a92593080d36e8c90ca2f00cfabf25
DinisSousa2003 <dinis.bessa.sousa@gmail.com>
2022-05-24 19:19:40 +0100

    Two pipes

--- cypher.c
+++ cypher.c
@@ -16,6 +16,7 @@ int main(int argc, char *argv[]){
     char line[LINESIZE];
 
     char * content = NULL;
+    char x;
     size_t line_size = 0;
     
     
@@ -30,21 +31,27 @@ int main(int argc, char *argv[]){
     }
 
     else if (pid > 0){
-        while(getline(&content,&line_size, stdin) > 0){
+        while((x = getc(stdin)) != EOF){
             /* parent */
-            printf("%s", content);
+            realloc(content, sizeof(char) * 2);
 
             /*write to pipe*/
             close(fd1[READ_END]);
-            snprintf(line, LINESIZE, "%s", content);
+            //snprintf(line, LINESIZE, "%s", content);
 
             if ((nbytes = write(fd1[WRITE_END], line, strlen(line))) < 0){
-                fprintf(stderr, "Unable to write to pipe: %s\n", strerror(errno));
+                //fprintf(stderr, "Unable to write to pipe: %s\n", strerror(errno));
             }
 
             close(fd1[WRITE_END]);
 
             /*read from pipe*/
+            close(fd2[WRITE_END]);
+            if ((nbytes = read(fd2[READ_END], line, LINESIZE)) < 0){
+                //fprintf(stderr, "Unable to read from pipe: %s\n", strerror(errno));
+            }
+            fprintf(stdout, "%s", line);
+            close(fd2[READ_END]);
         }
     }
 
@@ -56,13 +63,17 @@ int main(int argc, char *argv[]){
         /*read from parent*/
         close(fd1[WRITE_END]);
         if ((nbytes = read(fd1[READ_END], line, LINESIZE)) < 0){
-            fprintf(stderr, "Unable to read from pipe: %s\n", strerror(errno));
+            //fprintf(stderr, "Unable to read from pipe: %s\n", strerror(errno));
         }
         close(fd1[READ_END]);
         
         /*cypher the words*/
 
+        close(fd2[READ_END]);
         /* write message back to parent */
-        //write(STDOUT_FILENO, line, nbytes);
+        if ((nbytes = write(fd2[WRITE_END], line, strlen(line))) < 0){
+            //fprintf(stderr, "Unable to write to pipe: %s\n", strerror(errno));
+        }
+        close(fd2[WRITE_END]);
     }
 }
