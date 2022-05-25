060c9d3820b05d06dad36df5d1fc904d7e75b0ea
DinisSousa2003 <dinis.bessa.sousa@gmail.com>
2022-05-25 16:53:24 +0100

    Finish q3 with warnings

--- cypher.c
+++ cypher.c
@@ -32,11 +32,9 @@ int main(int argc, char *argv[])
         perror("fork");
         exit(EXIT_FAILURE);
     }
+
     else if (pid == 0) // children process
     {
-
-        char file_name[] = "cypher.txt";
-
         char end[2];
         memset(end, '\0', 2);
         end[1] = '1';
@@ -44,17 +42,36 @@ int main(int argc, char *argv[])
         close(ptoc[WRITE_END]);
         close(ctop[READ_END]);
 
-        FILE * cypher_file = open(file_name, "r");
+        FILE * cypher_file;
+        char file_name[11] = "cypher.txt";
+        char * line = NULL;
+        size_t len = 0;
 
-        while (fscanf(cypher_file, "%s %s\n", ) > 0)
-        {
-            /* code */
+        char **strings1;
+        char **strings2;
+        int numberOfElements = 0;
+
+        cypher_file = fopen(file_name, "r");
+        if(cypher_file == NULL){
+            exit (EXIT_FAILURE);
         }
-        
 
-        
-  
-        //int counter = 1000;
+        while (getline(&line, &len, cypher_file) > 0)
+        {
+            numberOfElements++;
+            strings1 = realloc(strings1, numberOfElements * sizeof(char*));
+            strings2 = realloc(strings2, numberOfElements * sizeof(char*));
+
+            char *token = strtok(line, " ");
+            strings1[numberOfElements - 1] = malloc(strlen(token) * sizeof(char));
+            strcpy(strings1[numberOfElements - 1], token);
+            
+            token = strtok(NULL, " ");
+            token[strcspn(token, "\r\n")] = '\0'; 
+            strings2[numberOfElements - 1] = malloc(strlen(token) * sizeof(char));
+            strcpy(strings2[numberOfElements - 1], token);
+        }
+
         while (1)
         {
             int r2 = read(ptoc[READ_END], buffer, MSGSIZE);
@@ -63,6 +80,16 @@ int main(int argc, char *argv[])
             if(buffer[1] == end[1] && buffer[0] == end[0]) break;
 
             /*code goes here*/
+            for(int i = 0; i < numberOfElements; i++){
+                if(strcmp(buffer, strings1[i]) == 0){
+                    strcpy(buffer, strings2[i]);
+                    break;
+                }
+                if(strcmp(buffer, strings2[i]) == 0){
+                    strcpy(buffer, strings1[i]);
+                    break;
+                }
+            }
 
         
             /*code ends here*/
@@ -121,7 +148,7 @@ int main(int argc, char *argv[])
         while(it_counter--){
             //char buffer[MSGSIZE];
             //memset(buffer, '\0', MSGSIZE);
-            int r1 = read(ctop[0],buffer , MSGSIZE);
+            read(ctop[0],buffer , MSGSIZE);
             //printf("r1: %d\n", r1);
             fprintf(stdout, "%s", &buffer[0]);
             
